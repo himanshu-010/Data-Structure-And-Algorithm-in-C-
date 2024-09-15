53980d6d30c0c7446f5c3ed97478a691da8b7d40
Himasnhu Kumar <himanshukumarmpr09@gamil.com>
2024-09-15 22:44:28 +0530

    This is normal CharArray

---
+++ CharArray/fc_in_charArray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+    // define char array
+    char name[50];
+
+    cout<<"Enter Your Full Name  ->> "<<endl;
+
+    cin>>name;
+
+    cout<<"Your Name is  -- >  "  <<name;
+
+
+
+}
